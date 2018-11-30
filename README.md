5f49ee2782a6f044d1413fab8dc1494c50fdfa46
Kitty <39654422+kat3ng@users.noreply.github.com>
2018-11-29 23:54:16 -0600

    readme complete

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# bamazon
-A Storefront CLI
+# The Leftorium: A Storefront CLI
+---
+- **For:** Northwestern Coding Bootcamp
+- **Developer:** Kat Eng
+- **Deployment Date** 11/29/2018
+- **Built With** Node.js, Javascript, mySQL
+- **Demo**
+
+
+### Description & Requirements
+---
+The Leftorium is an Amazon like CLI platform that allows users to view items, choose from the mySQL database inventory and calculate the total for "purchases".
+
+
+
+**Before you get started, make sure you have these node packages installed:**
+
+1. **Inquirer:** - A collection of common interactive command line user interfaces that can prompt and then take user input to capture, confirm, or validate.
+
+     *Command Line: 'npm install inquirer'*
+
+2. **Table:** - A command line module, that allows you to produces a string that represents array data in a text table in the command line.
+
+    *Command Line: 'npm install table'*
+
+3. **mysql:** - This is a node.js driver for mysql. It is written in JavaScript, does not require compiling, and is 100% MIT licensed.
+
+    *npm i mysql'*
+
+
+Read more about these methods [here](https://www.npmjs.com/)
+
+
+
+### Functionality
+--- 
+1. displayProducts()
+
+    Queries the database for all items, departments and prices and displays the to the console using the table module.
+
+    ![image of concertThis](/images/concertThis.png)
+    
+2. userOrder()
+
+   Uses inquirer to prompt the user for an item ID and a quantity and then validates them.
+
+    ![image of spotifyThis](/images/spotifyThis.png)
+
+
+3. checkQuantity()
+ 
+ Checks the quantity of the users chosen item and determines if the order can be filled. If it can it executes the updateInventory(), if it cannot it prompts the user to try again. 
+    ![image of movie-this](/images/movie-this.png)
+
+
+4. updateInventory() 
+
+If the user order can be filled, this function updates the inventory.
+
+    ![image of do-this](/images/do-this.png)
+
+5. calculateTotal() 
+
+This function calculates the users total for their order and displays it to the console.
+
+    ![image of do-this](/images/do-this.png)
+
+
+### Notes
+---
+This is my first back end project and I am super happy with it. If I were to go back I would probably try to implement node geocoder to translate the coordinates into an address. It might be something that I come back to after the course is finished.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,54 +21,84 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log(`We're connected, captain...`);
-    // afterConnection();
+    console.log(`\nWELCOME TO THE LEFTORIUM! WE'RE HERE TO SERVE YOUR MOST VITAL LEFT HANDED NEEDS!\n`);
     displayProducts();
 });
 
 
 function displayProducts() {
-    connection.query("SELECT * FROM products", function (err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(`| ${res[i].id} | ${res[i].product_name}  | ${res[i].department_name} | ${res[i].price}`);
-            console.log(`+--------------------------------------------------------------------|`);
-        }
+    const query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
+        const data = res.map(res => [`ID: ${res.id}`, res.product_name, `$ ${res.price}`]);
+        console.log(`Here are the available items...\n\n${table(data)}`);
+        const length = res.length;
+        // console.log(length);
+        userOrder(length);
     });
-    console.log(`displayProducts(), awaiting orders...`);
+    // connection.end();
 }
 
 
+function userOrder(length) {
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Enter the product id of the item you would like to order",
+        // Check to see if the user input is a number
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+                console.log("Please enter a vaid ID number.")
+            }
+            return false;
+        }
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like?"
 
+    }]).then(answers => {
+        const orderQuery = `SELECT * FROM products WHERE id = ${answers.id}`;
+        connection.query(orderQuery, function (err, res) {
+                // console.log(res);
+                const userQuantity = answers.quantity;
+                checkQuantity(userQuantity);
+            }
+            // userOrder();
+        )
 
+        function checkQuantity(userQuantity) {
+            quantityQuery = `SELECT * FROM products WHERE id = ${answers.id}`;
+            connection.query(quantityQuery, function (err, res) {
+                if (err) {
+                    throw err
+                };
+                if (res[0].stock_quantity >= userQuantity) {
+                    console.log(`You've chosen: ${userQuantity} units of product ${answers.id}`);
+                    const remainingQuantity = res[0].stock_quantity - userQuantity;
+                    calculateTotal(userQuantity);
+                } else {
+                    console.log(`I'm sorry, the maximum amount available is ${res[0].stock_quantity}.`);
+                    userOrder();
+                }
 
-const userOrder = () => {
-    // create an inquirer prompt that asks...
-    // what is the ID of the item you would like to purchase
-    // how many units of that item would you like to purchase?
-    // displayProducts();
-    // checkQuantity();
-    // orderTotal();
-    console.log(`userOrder(), awaiting orders...`);
-}
+            });
 
-const checkQuantity = () => {
-    // search quantity
-    // if units in database is >= to userOrder {
-    // console.log(`congrats! We can fill your order`)
-    // item -- in database;
-    // display orderTotal
-    // } else {
-    // console.log(`Insufficient Quantity!`)
-    // userOrder();
-    // }
-    console.log(`checkQuantity(), awaiting orders...`);
-}
+        }
 
-let orderTotal = () => {
-    // userOrder  + userOrder = orderTotal
-    console.log(`orderTotal(), awaiting orders...`);
-}
+        function updateInventory() {
+            let query = `UPDATE products SET ${} `
 
-// userOrder();
-// checkQuantity();
-// orderTotal();
+        }
+
+        function calculateTotal(userQuantity) {
+            let query = `SELECT * FROM products where id = ${answers.id}`;
+            connection.query(query, function (err, res) {
+                console.log(`Your total will be $${(res[0].price * userQuantity).toFixed(2)}`);
+                if (err) {
+                    throw err
+                };
+            })
+        }
+    })
+};
